32156cdc921593a479baed523122e2143f78cda7
abdullah <anabanmi@hotmail.com>
2019-02-10 16:47:25 +0300

    list/view impl

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,35 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
+    
     context = {
-
+    	
+    	"my_list": [{
+    		"restaurant_name": "foo1",
+    		"food_type": "oof1",
+    	},
+    	
+    	{
+    		"restaurant_name": "foo2",
+    		"food_type": "oof2",
+    	},
+    	
+    	{
+    		"restaurant_name": "foo3",
+    		"food_type": "oof3",
+    	}],
     }
+
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
+        "my_object": {
+            "restaurant_name": "foo1",
+            "food_type": "foo2",
+            "fav_rest": "fav1"
+        }
     }
     return render(request, 'detail.html', context)
